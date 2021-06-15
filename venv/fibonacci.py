5a9a50b2e9a3379d8a6e815d0d7e85f0968e16b4
joethliv <76746368+joethliv@users.noreply.github.com>
2021-06-15 12:13:32 +0300

    Delete venv directory

--- venv/fibonacci.py
+++
@@ -1,8 +0,0 @@
-fibonacci_sequence = [1, 1]
-upper_bound = int(input("Input highest number:\n"))
-i = 1
-print(f"\n{fibonacci_sequence[0]}\n{fibonacci_sequence[1]}")
-while fibonacci_sequence[i] <= upper_bound:
-    fibonacci_sequence.append(fibonacci_sequence[i-1] + fibonacci_sequence[i])
-    print(fibonacci_sequence[i]);
-    i += 1
--- venv/freefall.py
+++
@@ -1,6 +0,0 @@
-import math
-
-height = float(input("Input height here:\n"))
-g = 9.81
-time_in_s = math.sqrt(2*height/g)
-print(f"Time of free fall: {time_in_s}")
