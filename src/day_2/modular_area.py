68b38550a753b2da9bd312854cf01cefa0dee3a0
loremipsum510 <76746368+loremipsum510@users.noreply.github.com>
2021-06-16 12:53:29 +0300

    .gitignore created

--- src/day_2/modular_area.py
+++ src/day_2/modular_area.py
@@ -16,20 +16,29 @@ def calculate_area_triangle(a, b, c):
     s = calculate_semiparameter(a, b, c)
     return math.sqrt(s*(s-a)*(s-b)*(s-c))
 
-# Input 4 points and turn them into 4 lists of length 2, where the first element is the x-coordinate and the second element the y-coordinate
-points = input_points(4)
+def calculate_area_quadrilateral(p=None):
+    if p is None:
+        points = input_points(4)
+    else:
+        points = p
 
-# Calculations of distances between the points by the Pythagorean Theorem
-distances = []
-for i in range(3):
-    distances.append(calculate_distance_between_points(points[i], points[i+1]))
-distances.append(calculate_distance_between_points(points[3], points[0]))
-distances.append(calculate_distance_between_points(points[0], points[2]))
+    # Calculations of distances between the points by the Pythagorean Theorem
+    distances = []
+    for i in range(3):
+        distances.append(calculate_distance_between_points(points[i], points[i + 1]))
+    distances.append(calculate_distance_between_points(points[3], points[0]))
+    distances.append(calculate_distance_between_points(points[0], points[2]))
 
-# Calculation of both areas by Heron's Theorem
-area_1 = calculate_area_triangle(distances[0], distances[1], distances[4])
-area_2 = calculate_area_triangle(distances[2], distances[3], distances[4])
+    # Calculation of both areas by Heron's Theorem
+    area_1 = calculate_area_triangle(distances[0], distances[1], distances[4])
+    area_2 = calculate_area_triangle(distances[2], distances[3], distances[4])
 
-area = round(area_1 + area_2, 10)
+    area = round(area_1 + area_2, 10)
 
-print(f"Area: {area}")
+    print(f"Area: {area}")
+    return area
+
+
+
+
+
