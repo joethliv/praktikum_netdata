061bb731caed292475f54ce44a63fd247a2a07d5
loremipsum510 <76746368+loremipsum510@users.noreply.github.com>
2021-06-16 12:59:22 +0300

    test of modular area finished

--- tests/test_modular_area.py
+++ tests/test_modular_area.py
@@ -2,3 +2,12 @@ import src.day_2.modular_area as area
 
 def test_calculate_distance_between_points():
     assert area.calculate_distance_between_points([0, 0], [3, 4]) == 5
+
+def test_calculate_semiparameter():
+    assert area.calculate_semiparameter(1, 2 , 3) == 3
+
+def test_calculate_area_triangle():
+    assert area.calculate_area_triangle(3, 4, 5) == 6
+
+def test_calculate_area_quadrilateral():
+    assert area.calculate_area_quadrilateral([[-1, 1], [-1, -1], [1, -1], [1, 1]]) == 4
